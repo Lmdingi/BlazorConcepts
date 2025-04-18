5a0ab4a08f8cfbd30534a1f4d94d49e6d6641a50
Lmdingi <lmdingi4@gmail.com>
2025-04-18 15:23:06 +0200

    ServerManagement: using implicit expression (@varname)

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,23 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {     
+        // props
+        public int ServerId { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+        public bool IsOnline { get; set; }
+
+        // fields
+
+        // ctors
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0? false : true;
+        }
+
+        // methods
+    }
+}
